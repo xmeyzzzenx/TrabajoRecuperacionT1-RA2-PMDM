f5a7ba78acb66a6ceaca11bf5aa8acdb3273e3cd
Ximena Meyzen <xmeyzzzenx@gmail.com>
2026-04-14 15:42:15 +0200

    Primer commit PMDM ORDINARIA

---
+++ app/src/main/java/com/ximena/trabajorecuperaciont1_ra2_pmdm/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ximena.trabajorecuperaciont1_ra2_pmdm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ximena.trabajorecuperaciont1_ra2_pmdm.ui.theme.TrabajoRecuperacionT1RA2PMDMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TrabajoRecuperacionT1RA2PMDMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TrabajoRecuperacionT1RA2PMDMTheme {
+        Greeting("Android")
+    }
+}
