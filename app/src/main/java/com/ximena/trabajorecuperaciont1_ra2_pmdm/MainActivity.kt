05ac60998979aed85a69c95dd90418e34472f6a9
Ximena Meyzen <xmeyzzzenx@gmail.com>
2026-04-17 13:06:07 +0200

    Limpieza de MainActivity y eliminación de código de ejemplo

--- app/src/main/java/com/ximena/trabajorecuperaciont1_ra2_pmdm/MainActivity.kt
+++ app/src/main/java/com/ximena/trabajorecuperaciont1_ra2_pmdm/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.ximena.trabajorecuperaciont1_ra2_pmdm.ui.theme.TrabajoRecuperacionT1RA2PMDMTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +14,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TrabajoRecuperacionT1RA2PMDMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrabajoRecuperacionT1RA2PMDMTheme {
-        Greeting("Android")
-    }
+fun MainScreen() {
+    Text(text = "Hola")
 }
