0751ebc32e4e9d8120290f62332826526ce75b81
Ximena Meyzen <xmeyzzzenx@gmail.com>
2026-04-18 17:02:45 +0200

    Añadido Box en ProfileScreen para mejorar alineación y cumplir layouts

--- app/src/main/java/com/ximena/trabajorecuperaciont1_ra2_pmdm/screens/ProfileScreen.kt
+++ app/src/main/java/com/ximena/trabajorecuperaciont1_ra2_pmdm/screens/ProfileScreen.kt
@@ -1,7 +1,6 @@
 package com.ximena.trabajorecuperaciont1_ra2_pmdm.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,18 +11,23 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun ProfileScreen(navController: NavController) {
 
-    Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(16.dp)
+        ){
+            Text("Pantalla Perfil")
 
-    ){
-        Text("Pantalla Perfil")
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = {
-            navController.popBackStack()
-        }) {
-            Text("Volver")
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text("Volver")
+            }
         }
     }
 }
