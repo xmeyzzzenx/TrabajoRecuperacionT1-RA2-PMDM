7b591fe59f3e0089d6036e6cc965f74112311c48
Ximena Meyzen <xmeyzzzenx@gmail.com>
2026-04-18 15:24:40 +0200

    Implementado formulario con campos de entrada (TextField)

--- app/src/main/java/com/ximena/trabajorecuperaciont1_ra2_pmdm/screens/FormScreen.kt
+++ app/src/main/java/com/ximena/trabajorecuperaciont1_ra2_pmdm/screens/FormScreen.kt
@@ -1,10 +1,8 @@
 package com.ximena.trabajorecuperaciont1_ra2_pmdm.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
 import androidx.navigation.NavController
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -12,12 +10,34 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun FormScreen(navController: NavController) {
 
+    var titulo by remember { mutableStateOf("") }
+    var descripcion by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Text("Pantalla Formulario")
+        Text("Formulario", style = MaterialTheme.typography.titleLarge)
+
+        OutlinedTextField(
+            value = titulo,
+            onValueChange = { titulo = it },
+            label = { Text("Título") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = descripcion,
+            onValueChange = { descripcion = it },
+            label = { Text("Descripción") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(onClick = {
+            // validación
+        }) {
+            Text("Guardar")
+        }
 
         Button(onClick = {
             navController.popBackStack()
