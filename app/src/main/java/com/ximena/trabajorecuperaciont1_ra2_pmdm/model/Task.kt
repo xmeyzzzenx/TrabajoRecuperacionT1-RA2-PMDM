d6af1daa91ecf6eb64f320e6fe1e776ed1950576
Ximena Meyzen <xmeyzzzenx@gmail.com>
2026-05-10 18:25:19 +0200

    Cambiado modelo de Note a Task con estado de completado

--- app/src/main/java/com/ximena/trabajorecuperaciont1_ra2_pmdm/model/Task.kt
+++ app/src/main/java/com/ximena/trabajorecuperaciont1_ra2_pmdm/model/Task.kt
@@ -1,6 +1,9 @@
 package com.ximena.trabajorecuperaciont1_ra2_pmdm.model
 
+// clase que representa una tarea con titulo, descripcion
+// y un estado para saber si esta completada o no
 data class Task(
     val title: String,
-    val description: String
+    val description: String,
+    val isCompleted: Boolean = false
 )
